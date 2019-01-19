9ff1fe63735b1e471613e8ec46fddf6397981d48
Ali Jaya Meilio <alijayameilio@gmail.com>
2019-01-19 01:44:09 +0100

    debat

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,31 @@
 <template>
-  <el-main class="home">
+  <el-main class="home center">
     <CanVote />
-    <h1>Pemilu PPI Prancis 2018 - 2019</h1>
-    <p>
-      <router-link to="/ridho"><el-button>Info tentang 1. Ridho dan Avira</el-button></router-link>
-    </p>
-    <p>
-      <router-link to="/hazem"><el-button>Info tentang 2. Hazem dan Yulia</el-button></router-link>
-    </p>
-    <p><img src="@/assets/calon.jpg" class="full-width" /></p>
-    <template v-if="!$store.isLoggedIn">
-      <h2>Please log in!</h2>
-      <router-link to="/login"><el-button type="primary">Go to Login Page</el-button></router-link>
-    </template>
-    <template v-else>
-      <h2>Please vote!</h2>
-      <router-link to="/user"><el-button type="primary">Go to User Page</el-button></router-link>
-    </template>
+    <article class="article">
+      <h1>Pemilu PPI Prancis 2018 - 2019</h1>
+      <p>
+        <router-link to="/ridho"><el-button>Info tentang 1. Ridho dan Avira</el-button></router-link>
+      </p>
+      <p>
+        <router-link to="/hazem"><el-button>Info tentang 2. Hazem dan Yulia</el-button></router-link>
+      </p>
+      <p>
+        <img src="@/assets/calon.jpg" class="full-width" />
+      </p>
+      <h1>Debat Pemilu PPI Prancis 2018 - 2019 (20 Januari 2019, Jam 8 Malam)</h1>
+      <div class="video-wrapper">
+        <iframe src="https://www.youtube.com/embed/mFv59fJbiL0" frameborder="0"  allowfullscreen></iframe>
+      </div>
+
+      <template v-if="!$store.isLoggedIn">
+        <h2>Please log in!</h2>
+        <router-link to="/login"><el-button type="primary">Go to Login Page</el-button></router-link>
+      </template>
+      <template v-else>
+        <h2>Please vote!</h2>
+        <router-link to="/user"><el-button type="primary">Go to User Page</el-button></router-link>
+      </template>
+    </article>
   </el-main>
 </template>
 
@@ -33,7 +42,29 @@ export default {
 </script>
 
 <style scoped>
+.video-wrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+}
+.video-wrapper > iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 .full-width {
   width: 100%;
+}
+.article {
+  max-width: 100%;
+  width: 40em;
+  line-height: 2em;
+}
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
